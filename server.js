c2b676b9c10dbe624bbff5db870b86ab1498a4ac
Yordi <yordicartagena24@gmail.com>
2018-09-24 18:13:56 -0400

    Primer commit

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const app = express();
+
+const hbs = require('hbs');
+require('./hbs/helper');
+
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/public'));
+
+//Express HBS engine
+hbs.registerPartials(__dirname + '/views/parciales');
+app.set('view engine', 'hbs');
+
+
+app.get('/', (req, res) => {
+
+    res.render('home', {
+        nombre: "Yordi Cartagena"
+
+    });
+});
+
+app.get('/about', (req, res) => {
+
+    res.render('about', {
+        nombre: "Yordi Cartagena"
+    });
+});
+
+// app.get('/data', (req, res) => {
+//     res.send('Hola data');
+// })
+
+app.listen(port, () => {
+    console.log(`Escuchando peticiones en el puerto ${ port }`);
+});
